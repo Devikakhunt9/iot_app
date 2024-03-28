0ce3917a720b0d5d88145fdaff55fa794a252048
Jayaprakash S <70194564+jayaprakash8088@users.noreply.github.com>
2024-03-28 08:15:12 +0530

    working on controllers

--- lib/presentation/Homepage/HomePage/controller/homeController.dart
+++ lib/presentation/Homepage/HomePage/controller/homeController.dart
@@ -1,8 +1,17 @@
 
 import 'package:aagash_s_application1/core/app_export.dart';
+import 'package:aagash_s_application1/data/models/responseModel/home_page_response_model/energy_response_model.dart';
 
 class HomeController extends GetxController{
 
   RxInt navigationIndex = 0.obs;
+  RxInt totalEnergy=0.obs;
+
+  getTotalEnergy(){
+    EnergyResponseModel? energyResponseModel;
+    totalEnergy.value=energyResponseModel!.energy as int;
+    update();
+  }
+
 
 }
